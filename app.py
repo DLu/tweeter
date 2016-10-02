b2cfe6be845c19548ceded741625e48a57097301
David V. Lu <davidvlu@gmail.com>
2016-10-01 22:15:01 -0400

    Read and write tweets

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
+#!/usr/bin/python
 from flask import Flask, render_template, request, jsonify
+import tweeter
 app = Flask(__name__)
+
+class Reader:
+    def __init__(self):
+        self.twit = tweeter.Tweeter()
+        self.skipped = set()
+        self.current = None
+        
+    def post(self, read):
+        if self.current is None:
+            return
+        if read:
+            self.twit.mark_as_read(self.current)
+        else:
+            self.skipped.add(self.current['id_str'])
+        self.current = None
+        
+    def get_tweet(self):
+        for lname, tweets in self.twit.tweets.iteritems():
+            for tweet in tweets:
+                if tweet['id_str'] in self.skipped:
+                    continue
+                else:
+                    self.current = tweet
+                    return self.current['id_str']
+
+reader = Reader()
 
 @app.route('/')
 def index():
@@ -7,10 +35,13 @@ def index():
 
 @app.route('/interact')
 def interact():
-    try:
-        return jsonify(id='779659915510046720')
-    except Exception as e:
-        return str(e)
+    if 'read' in request.args:
+        reader.post(request.args.get('read')=='true')
+    id = reader.get_tweet()
+    return jsonify(id=id)
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        app.run()
+    finally:
+        reader.twit.write()
